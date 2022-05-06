26df9781f89947cdfe6f39ba8a80bf5f66efdb3d
griselamundo <102989061+griselamundo@users.noreply.github.com>
2022-05-06 09:15:12 -0400

    Create Gradiantes de color.py

---
+++ Gradiantes de color.py
@@ -0,0 +1,16 @@
+### Modifique su código aquí ###
+Rect(25, 25, 100, 100, relleno=gradiente ('rojo', 'amarillo', inicio='superior-izquierda'))
+Rect(150, 25, 100, 100,  relleno=gradiente ('rojo', 'amarillo', inicio='superior'))
+Rect(275, 25, 100, 100,  relleno=gradiente ('rojo', 'amarillo', inicio='superior-derecha'))
+
+# Aquí está el primero de los tres rectángulos en la fila del medio.
+### Modifique y ponga su código aquí ###
+Rect(25, 150, 100, 100, relleno=gradiente ('rojo', 'amarillo', inicio='izquierda'))
+Rect(150,150,100,100, relleno=gradiente ('rojo', 'amarillo', inicio='centro'))
+Rect(275,150,100,100, relleno=gradiente ('rojo', 'amarillo', inicio='derecha'))
+
+# Aquí está el primero de los tres rectángulos en la fila inferior.
+### Modifique y ponga su código aquí ###
+Rect(25, 275, 100, 100,relleno=gradiente ('rojo', 'amarillo', inicio='inferior-izquierda'))
+Rect(150, 275, 100, 100,relleno=gradiente ('rojo', 'amarillo', inicio='inferior'))
+Rect(275, 275, 100, 100,relleno=gradiente ('rojo', 'amarillo', inicio='inferior-derecha'))
